de3a912049893fe9e9694e66ea4ebf657422f823
Jay Paik <jpaik@seas.upenn.edu>
2012-02-13 04:32:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expenseapp2::Application.config.secret_token = 'fa0cfed7192df8f4cbc4a3328185eebd331c2009a8f8d79b7a9c6a5ed8f3c2452b252743007f7bb41020d1514ea7e2f51c8c53e169ea0c1efc3b41f8a9f5bb9a'
