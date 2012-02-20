7d70ab1543e3f17c54fd1ec081842e9303cd7dad
Jay Paik <jpaik@seas.upenn.edu>
2012-02-20 07:02:03 -0500

    update

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.assets.compile = true
-
 # Initialize the rails application
 Expenseapp2::Application.initialize!
