1cf95146ad27ed141f2c60ad9d1b30fca412c35c
Jay Paik <jpaik@seas.upenn.edu>
2012-03-12 12:21:58 -0400

    devise update 2

--- db/migrate/20120312061224_add_devise_to_users.rb
+++ db/migrate/20120312061224_add_devise_to_users.rb
@@ -3,24 +3,24 @@ class AddDeviseToUsers < ActiveRecord::Migration
     remove_column :users, :email
     
     change_table(:users) do |t|
-      t.database_authenticatable :null => false
+      ###t.database_authenticatable :null => false
       ## Database authenticatable
-      #t.string :email,              :null => false, :default => ""
-      #t.string :encrypted_password, :null => false, :default => ""
-      t.recoverable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+      ###t.recoverable
       ## Recoverable
-      #t.string   :reset_password_token
-      #t.datetime :reset_password_sent_at
-      t.rememberable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      ###t.rememberable
       ## Rememberable
-      #t.datetime :remember_created_at
-      t.trackable
+      t.datetime :remember_created_at
+      ###t.trackable
       ## Trackable
-      #t.integer  :sign_in_count, :default => 0
-      #t.datetime :current_sign_in_at
-      #t.datetime :last_sign_in_at
-      #t.string   :current_sign_in_ip
-      #t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Encryptable
       # t.string :password_salt
